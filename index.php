a84aea19a24ccb85f839006aeb2fe6d709a3d2ca
Alfaneos <alfaneos@gmail.com>
2017-08-19 18:50:51 +0600

    9

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 
 require_once './include/DbHandler.php';
 require_once './include/PassHash.php';
-//require '././libs/Slim/Slim.php';
+require '././libs/Slim/Slim.php';
 //require './vendor/autoload.php';
 //require './include/simple_html_dom.php';
 \Slim\Slim::registerAutoloader();
