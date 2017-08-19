6e07183061901f9c74755302e6f598c29b6b72c3
Alfaneos <alfaneos@gmail.com>
2017-08-19 18:52:26 +0600

    10

--- index.php
+++ index.php
@@ -3,8 +3,8 @@
 require_once './include/DbHandler.php';
 require_once './include/PassHash.php';
 require '././libs/Slim/Slim.php';
-//require './vendor/autoload.php';
-//require './include/simple_html_dom.php';
+require './vendor/autoload.php';
+require './include/simple_html_dom.php';
 \Slim\Slim::registerAutoloader();
 
 $app = new \Slim\Slim();
